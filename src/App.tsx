14027a9ef73c01f3ec33212e3e9136557a4d806d
Murilo Thom <murilothom.2002@hotmail.com>
2024-06-17 13:45:07 -0300

    adicionado axios e react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './global.css'
 
+import { QueryClientProvider } from '@tanstack/react-query'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
 
 import { ThemeProvider } from './components/theme/theme-provider'
+import { queryClient } from './lib/react-query'
 import { router } from './routes'
 
 export const App = () => {
@@ -13,7 +15,10 @@ export const App = () => {
       <HelmetProvider>
         <Helmet titleTemplate="%s | pizza.shop" />
         <Toaster richColors position="top-right" />
-        <RouterProvider router={router} />
+
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
       </HelmetProvider>
     </ThemeProvider>
   )
