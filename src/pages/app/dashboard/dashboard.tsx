c450c1269f4b216dc787a70e9cb81e7566a6cf7e
Murilo Thom <murilothom.2002@hotmail.com>
2024-06-17 12:46:33 -0300

    grafico para produtos populares e receita

--- src/pages/app/dashboard/dashboard.tsx
+++ src/pages/app/dashboard/dashboard.tsx
@@ -4,6 +4,8 @@ import { DayOrdersAmountCard } from './day-orders-amount-card'
 import { CanceledOrdersAmountCard } from './month-canceled-orders-amount-card'
 import { MonthOrdersAmountCard } from './month-orders-amount-card'
 import { MonthRevenueCard } from './month-revenue-card'
+import { PopularProductsChart } from './popular-products-chart'
+import { RevenueChart } from './revenue-chart'
 
 export const Dashboard = () => {
   return (
@@ -17,6 +19,11 @@ export const Dashboard = () => {
           <MonthOrdersAmountCard />
           <DayOrdersAmountCard />
           <CanceledOrdersAmountCard />
+        </div>
+
+        <div className="grid grid-cols-9 gap-4">
+          <RevenueChart />
+          <PopularProductsChart />
         </div>
       </div>
     </>
