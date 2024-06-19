6ded41c49acc72326992b5be0b66273382e319af
Murilo Thom <murilothom.2002@hotmail.com>
2024-06-18 21:28:35 -0300

    mock pedido em entrega

--- src/api/dispatch-order.ts
+++ src/api/dispatch-order.ts
@@ -1,6 +1,6 @@
 import { api } from '../lib/axios'
 
-interface DispatchOrderDetailsParams {
+export interface DispatchOrderDetailsParams {
   orderId: string
 }
 
---
+++ src/api/mocks/dispatch-order-mock.ts
@@ -0,0 +1,15 @@
+import { http, HttpResponse } from 'msw'
+
+import { DispatchOrderDetailsParams } from '../dispatch-order'
+
+export const dispatchOrderMock = http.patch<
+  DispatchOrderDetailsParams,
+  never,
+  never
+>('/orders/:orderId/dispatch', async ({ params }) => {
+  if (params.orderId === 'error-order-id') {
+    return new HttpResponse(null, { status: 400 })
+  }
+
+  return new HttpResponse(null, { status: 204 })
+})
