e39861c80da0d6395cf0fc46ed9ec0eeeb2b3cb2
Murilo Thom <murilothom.2002@hotmail.com>
2024-06-19 11:16:20 -0300

    testes e2e sign-in sign-up

---
+++ test/sign-in.e2e-spec.ts
@@ -0,0 +1,35 @@
+import { expect, test } from '@playwright/test'
+
+test('sign in successfully', async ({ page }) => {
+  await page.goto('/sign-in', { waitUntil: 'networkidle' })
+
+  await page.getByLabel('E-mail').fill('example@email.com')
+
+  await page.getByRole('button', { name: 'Acessar painel' }).click()
+
+  const toast = page.getByText(
+    'Enviamos um link de autenticação para seu e-mail',
+  )
+
+  expect(toast).toBeVisible()
+})
+
+test('sign in with wrong credentials', async ({ page }) => {
+  await page.goto('/sign-in', { waitUntil: 'networkidle' })
+
+  await page.getByLabel('E-mail').fill('wrong@email.com')
+
+  await page.getByRole('button', { name: 'Acessar painel' }).click()
+
+  const toast = page.getByText('Ocorreu um erro, tente novamente mais tarde')
+
+  expect(toast).toBeVisible()
+})
+
+test('navigate to new restaurant page', async ({ page }) => {
+  await page.goto('/sign-in', { waitUntil: 'networkidle' })
+
+  await page.getByRole('link', { name: 'Novo estabelecimento' }).click()
+
+  expect(page.url()).toContain('/sign-up')
+})
