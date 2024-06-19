225e2db367810ac833ec81b341e2d296a3fa827a
Murilo Thom <murilothom.2002@hotmail.com>
2024-06-19 11:49:35 -0300

    corrigido teste e2e atualizar perfil

--- test/store-profile.e2e-spec.ts
+++ test/store-profile.e2e-spec.ts
@@ -19,5 +19,5 @@ test('update profile successfully', async ({ page }) => {
 
   await page.getByRole('button', { name: 'Cancelar' }).click()
 
-  expect(page.getByRole('button', { name: 'New Pizza' })).toBeVisible()
+  await expect(page.getByRole('button', { name: 'New Pizza' })).toBeVisible()
 })
