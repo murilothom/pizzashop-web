349a2a7e130e647d0e2b8e061d97ea2fe7911628
Murilo Thom <murilothom.2002@hotmail.com>
2024-06-18 21:10:37 -0300

    atualizar perfil

--- src/api/mocks/index.ts
+++ src/api/mocks/index.ts
@@ -3,12 +3,16 @@ import { setupWorker } from 'msw/browser'
 import { env } from '../../env'
 import { getDailyRevenueInPeriodMock } from './get-daily-revenue-in-period-mock'
 import { getDayOrdersAmountMock } from './get-day-orders-amount-mock'
+import { getManagedRestaurantMock } from './get-managed-restaurant-mock'
 import { getMonthCanceledOrdersAmountMock } from './get-month-canceled-orders-amount-mock'
 import { getMonthOrdersAmountMock } from './get-month-orders-amount-mock'
 import { getMonthRevenueMock } from './get-month-revenue-mock'
+import { getOrdersMock } from './get-orders-mock'
 import { GetPopularProductsMock } from './get-popular-products'
+import { getProfileMock } from './get-profile-mock'
 import { registerRestaurantMock } from './register-restaurant-mock'
 import { signInMock } from './sign-in-mock'
+import { updateProfileMock } from './update-profile-mock'
 
 export const worker = setupWorker(
   signInMock,
@@ -19,6 +23,10 @@ export const worker = setupWorker(
   getMonthRevenueMock,
   getDailyRevenueInPeriodMock,
   GetPopularProductsMock,
+  getProfileMock,
+  updateProfileMock,
+  getManagedRestaurantMock,
+  getOrdersMock,
 )
 
 export async function enableMSW() {
